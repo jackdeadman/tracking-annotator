ebeb53249445807d47e5a3aa996cd94201e33c8b
Jack Deadman <jdeadman1@sheffield.ac.uk>
2020-05-29 12:29:30 +0100

    Update serve_videos.sh

--- serve_videos.sh
+++ serve_videos.sh
@@ -1,6 +1,5 @@
 # Mount the video directory and serve them using a local server
-
-read -p "Enter Sharc username: " username
-sshfs $username@sharc:/fastdata/acp18jd/data/videos_converted videos
+mkdir -p videos
+sshfs acp18jd@sharc:/fastdata/acp18jd/data/videos_converted videos
 cd videos
 npx http-server
